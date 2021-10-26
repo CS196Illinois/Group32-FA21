bc6d604302c42269d16145078d9bfd9f88d08327
khalidalotaibi <khalid.al-otaibi@outlook.com>
2021-10-25 19:44:35 -0500

    develop basic player class Fixes #3

--- Project/group32/core/src/com/cs196/midcard/Game.java
+++ Project/group32/core/src/com/cs196/midcard/Game.java
@@ -5,10 +5,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 
 public class Game extends ApplicationAdapter {
 	SpriteBatch batch;
 	Music introMusic;
+	Texture character;
+	Player player;
+	OrthographicCamera camera;
 
 	@Override
 	public void create () {
@@ -21,13 +26,21 @@ public class Game extends ApplicationAdapter {
 		https://creativecommons.org/licenses/by/4.0/ */
 
 		introMusic = Gdx.audio.newMusic(Gdx.files.internal("music/Intromusic.mp3"));
+		introMusic.setLooping(true);
 		introMusic.play();
+
+		character = new Texture("mainCharacter.png");
+		camera = new OrthographicCamera();
+		player = new Player(character, camera);
+		player.setUpCamera();
+		player.setUpBatch();
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
+		player.render();
 		batch.end();
 	}
 	
