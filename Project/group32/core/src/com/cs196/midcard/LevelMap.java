8ce326a62da55702be5f734f4bd34567a378291e
Rohan <gvrohan11@gmail.com>
2021-10-29 04:09:59 -0500

    new java file: level map

---
+++ Project/group32/core/src/com/cs196/midcard/LevelMap.java
@@ -0,0 +1,17 @@
+package com.cs196.midcard;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class Map {
+    private Texture levelMap;
+
+    public void create() {
+        levelMap = new Texture(Gdx.files.internal("Level Map.jpg"));
+    }
+}
