4032f9662548ff5bf914e1919160e8da11bb712c
Rohan G <gvrohan11@gmail.com>
2021-10-29 15:09:31 -0500

    edit level map

--- Project/group32/core/src/com/cs196/midcard/LevelMap.java
+++ Project/group32/core/src/com/cs196/midcard/LevelMap.java
@@ -7,14 +7,28 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Rectangle;
 
 public class LevelMap {
     private Texture levelMap;
     private Texture mainCharacter;
+    private Rectangle rectChar;
+    private Music music;
+    private OrthographicCamera camera;
 
     public void create() {
+        // load level map
         levelMap = new Texture(Gdx.files.internal("Level Map.jpg"));
+
+        // load character
         mainCharacter = new Texture(Gdx.files.internal("mainCharacter.png"));
+
+        // load music
+        music = Gdx.audio.newMusic(Gdx.files.internal("music/Intromusic.mp3"));
+        music.setLooping(true);
+        music.play();
+
+
 
     }
 }
