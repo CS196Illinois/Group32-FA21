1b9a7f7f4f68dd660a437b52ce02954bb5aea190
kingleo2003 <leo1720898845@gmail.com>
2021-10-29 14:10:49 -0500

    asset

--- Project/group32/core/src/com/cs196/midcard/Enemy.java
+++ Project/group32/core/src/com/cs196/midcard/Enemy.java
@@ -1,6 +1,14 @@
 package com.cs196.midcard;
 
-public class Enemy {
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 
+public class Enemy extends ApplicationAdapter {
+    private Texture enemyOne;
+    public void create() {
+        enemyOne = new Texture(Gdx.files.internal("Ogre.gif"));
+
+    }
 
 }
